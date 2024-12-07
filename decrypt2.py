05fe6eb63f8f6e29c30e317ceb29a50847c72b0b
ahmed-momen61 <aa2301532@tkh.edu.eg>
2024-12-07 10:27:35 +0200

    Update decrypt2.py

--- decrypt2.py
+++ decrypt2.py
@@ -5,16 +5,16 @@ def mod_inv(a, p):
     t, new_t = 0, 1
     r, new_r = p, a
     while new_r != 0:
-        quotient = r // new_r
-        t, new_t = new_t, t - quotient * new_t
-        r, new_r = new_r, r - quotient * new_r
+        dividing_result = r // new_r
+        t, new_t = new_t, t - dividing_result * new_t
+        r, new_r = new_r, r - dividing_result * new_r
     if r > 1:
         raise ValueError(f"{a} has no inverse modulo {p}")
     if t < 0:
         t = t + p
     return t
 
-# Exponentiation function (g power k mod p)
+# Exponentiation function (g^k mod p)
 def g_power_k_mod_p(base, exp, mod):
     result = 1
     for _ in range(exp):
